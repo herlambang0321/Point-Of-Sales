8ba9c753c166e628084884f9eaaaeacff12eb628
Herlambang Gunawan <herlambang88888888@gmail.com>
2022-11-11 01:08:19 +0700

    repair dashboard & tampilan users done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const { isLoggedIn } = require('../helpers/util');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = function (db) {
 
-module.exports = router;
+  router.get('/', isLoggedIn, function (req, res, next) {
+    res.render('users', {
+      user: req.session.user
+    });
+  });
+
+  return router;
+}
