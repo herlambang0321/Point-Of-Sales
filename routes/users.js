1f5089fb0afe7d493363d7bec447ba830b635946
Herlambang Gunawan <herlambang88888888@gmail.com>
2022-11-17 01:11:23 +0700

    repair done

--- routes/users.js
+++ routes/users.js
@@ -24,6 +24,32 @@ module.exports = function (db) {
     }
   });
 
+  router.get('/tableuser', async (req, res, next) => {
+    let params = []
+
+    if (req.query.search.value) {
+      params.push(`email ilike '%${req.query.search.value}%'`)
+    }
+    if (req.query.search.value) {
+      params.push(`name ilike '%${req.query.search.value}%'`)
+    }
+
+    const limit = req.query.length
+    const offset = req.query.start
+    const sortBy = req.query.columns[req.query.order[0].column].data
+    const sortMode = req.query.order[0].dir
+
+    const total = await db.query(`select count(*) as total from users${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
+    const data = await db.query(`select * from users${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
+    const response = {
+      "draw": Number(req.query.draw),
+      "recordsTotal": total.rows[0].total,
+      "recordsFiltered": total.rows[0].total,
+      "data": data.rows
+    }
+    res.json(response)
+  })
+
   router.get('/add', isLoggedIn, async function (req, res, next) {
     try {
 
