23156cf9865aa3157bad7c039cc083001bc07c6f
Herlambang Gunawan <herlambang88888888@gmail.com>
2022-11-28 23:53:58 +0700

    router purchases, tampilan add purchases done

--- routes/purchases.js
+++ routes/purchases.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path')
 const { isLoggedIn } = require('../helpers/util');
+const { currencyFormatter } = require('../public/javascripts/util')
 const moment = require('moment');
 
 /* GET purchases listing. */
@@ -17,6 +18,7 @@ module.exports = function (db) {
                 successMessage: req.flash('successMessage'),
                 path: req.originalUrl,
                 title: 'POS Purchases',
+                currencyFormatter,
                 moment
             });
         } catch (err) {
@@ -37,7 +39,7 @@ module.exports = function (db) {
         const sortMode = req.query.order[0].dir
 
         const total = await db.query(`select count(*) as total from purchases${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
-        const data = await db.query(`select * from purchases${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
+        const data = await db.query(`select p.*, s.* from purchases as p left join suppliers as s on p.supplier = s.supplierid${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
         const response = {
             "draw": Number(req.query.draw),
             "recordsTotal": total.rows[0].total,
@@ -76,6 +78,51 @@ module.exports = function (db) {
             })
         } catch (err) {
             res.send(err);
+        }
+    });
+
+    router.post('/show/:invoice', isLoggedIn, async (req, res, next) => {
+        try {
+            const { totalsum, supplier } = req.body
+            await db.query('UPDATE purchases SET totalsum = $1, supplier = $2 WHERE invoice = $3', [totalsum, supplier, req.params.invoice])
+
+            req.flash('successMessage', 'Transaction Success!')
+            res.redirect('/purchases')
+        } catch (error) {
+            req.flash('error', 'Transaction Fail!')
+            return res.redirect('/purchases')
+        }
+    });
+
+    router.get('/goods/:barcode', isLoggedIn, async (req, res) => {
+        try {
+            const { rows } = await db.query('SELECT * FROM goods WHERE barcode = $1', [req.params.barcode]);
+
+            res.json(rows[0])
+        } catch (err) {
+            res.send(err)
+        }
+    });
+
+    router.post('/additem', isLoggedIn, async (req, res) => {
+        try {
+            const { invoice, itemcode, quantity } = req.body
+            await db.query('INSERT INTO purchaseitems (invoice, itemcode, quantity)VALUES ($1, $2, $3) returning*', [invoice, itemcode, quantity]);
+            const { rows } = await db.query('SELECT * FROM purchases WHERE invoice = $1', [invoice])
+
+            res.json(rows[0])
+        } catch (err) {
+            res.send(err)
+        }
+    });
+
+    router.get('/details/:invoice', isLoggedIn, async (req, res, next) => {
+        try {
+            const { rows: data } = await db.query('SELECT purchaseitems.*, goods.name FROM purchaseitems LEFT JOIN goods ON purchaseitems.itemcode = goods.barcode WHERE purchaseitems.invoice = $1 ORDER BY purchaseitems.id', [req.params.invoice])
+
+            res.json(data)
+        } catch (err) {
+            res.send(err)
         }
     });
 
