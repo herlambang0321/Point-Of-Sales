57e5f646d936b1b211029f12c672ef73229e2c0b
Herlambang Gunawan <herlambang88888888@gmail.com>
2022-12-02 20:55:34 +0700

    router & tampilan form add, edit(sales), repair form(purchases) done

--- routes/sales.js
+++ routes/sales.js
@@ -49,5 +49,85 @@ module.exports = function (db) {
         res.json(response)
     })
 
+    router.get('/create', async function (req, res, next) {
+        try {
+            const { userid } = req.session.user
+            const { rows } = await db.query('INSERT INTO sales(totalsum, operator) VALUES(0, $1) returning *', [userid])
+            res.redirect(`/sales/show/${rows[0].invoice}`)
+        } catch (err) {
+            res.send(err)
+        }
+    });
+
+    router.get('/show/:invoice', isLoggedIn, async (req, res, next) => {
+        try {
+            const sales = await db.query('SELECT s.*, c.* FROM sales AS s LEFT JOIN customers AS c ON s.customer = c.customerid where invoice = $1', [req.params.invoice])
+            const users = await db.query('SELECT * FROM users ORDER BY userid')
+            const { rows: goods } = await db.query('SELECT barcode, name FROM goods ORDER BY barcode')
+            const { rows } = await db.query('SELECT * FROM customers ORDER BY customerid')
+
+            res.render('sales/form', {
+                path: req.originalUrl,
+                title: 'POS Sales',
+                user: req.session.user,
+                sales: sales.rows[0],
+                goods,
+                users,
+                customers: rows,
+                moment,
+            })
+        } catch (err) {
+            res.send(err);
+        }
+    });
+
+    router.post('/show/:invoice', isLoggedIn, async (req, res, next) => {
+        try {
+            const { totalsum, pay, change, customer } = req.body
+            const s = await db.query('UPDATE sales SET totalsum = $1, pay = $2, change = $3, customer = $4 WHERE invoice = $5', [totalsum, pay, change, customer, req.params.invoice])
+            console.log(s);
+
+            req.flash('successMessage', 'Transaction Success!')
+            res.redirect('/sales')
+        } catch (error) {
+            console.log(error);
+            req.flash('error', 'Transaction Fail!')
+            return res.redirect('/sales')
+        }
+    });
+
+
+    router.get('/goods/:barcode', isLoggedIn, async (req, res) => {
+        try {
+            const { rows } = await db.query('SELECT * FROM goods WHERE barcode = $1', [req.params.barcode]);
+
+            res.json(rows[0])
+        } catch (err) {
+            res.send(err)
+        }
+    });
+
+    router.post('/additem', isLoggedIn, async (req, res) => {
+        try {
+            const { invoice, itemcode, quantity } = req.body
+            await db.query('INSERT INTO saleitems (invoice, itemcode, quantity) VALUES ($1, $2, $3) returning*', [invoice, itemcode, quantity]);
+            const { rows } = await db.query('SELECT * FROM sales WHERE invoice = $1', [invoice])
+
+            res.json(rows[0])
+        } catch (err) {
+            res.send(err)
+        }
+    });
+
+    router.get('/details/:invoice', isLoggedIn, async (req, res, next) => {
+        try {
+            const { rows: data } = await db.query('SELECT saleitems.*, goods.name FROM saleitems LEFT JOIN goods ON saleitems.itemcode = goods.barcode WHERE saleitems.invoice = $1 ORDER BY saleitems.id', [req.params.invoice])
+
+            res.json(data)
+        } catch (err) {
+            res.send(err)
+        }
+    });
+
     return router;
 }
